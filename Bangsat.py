6445d7b9ea587a81708d383ce2f375fd182dc3ac
☆ Raka ☆ ™︻®╤───────═◍➤ <93072215+Bangsat-XD@users.noreply.github.com>
2021-11-05 12:18:04 +0700

    Update Bangsat.py

--- Bangsat.py
+++ Bangsat.py
@@ -14,7 +14,7 @@ print (bacot)
 
 u = raw_input('\033[1;97m[\033[1;91m×\033[1;97m] Username : ')
 p = raw_input('\033[1;97m[\033[1;91m×\033[1;97m] Password : ')
-pahrul = requests.get('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email='+u+'&locale=en_US&password='+p+'&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
+bangsat = requests.get('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email='+u+'&locale=en_US&password='+p+'&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
 
 lop = bangsat.content
 croot = json.loads(lop)
